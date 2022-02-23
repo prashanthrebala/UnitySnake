52051be8cf60c5a3704985ebabdd6985e8f75138
prashanthrebala <prashanth.rebala@gmail.com>
2022-02-24 04:35:38 +0530

    Snake basic implementation

---
+++ Assets/Scenes/SnakeMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+
+    private void Start() {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
+    private void Update() { 
+
+        if(Input.GetKeyDown(KeyCode.W)) {
+            _direction = Vector2.up;
+        } else if(Input.GetKeyDown(KeyCode.A)) {
+            _direction = Vector2.left;
+        } else if(Input.GetKeyDown(KeyCode.S)) {
+            _direction = Vector2.down;
+        } else if(Input.GetKeyDown(KeyCode.D)) {
+            _direction = Vector2.right;
+        }
+
+    }
+
+    private void FixedUpdate() { 
+
+        for(int i = _segments.Count - 1; i > 0; i--) {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+        _segments[0].position = new Vector3(
+            Mathf.Round(this.transform.position.x) + this._direction.x,
+            Mathf.Round(this.transform.position.y) + this._direction.y,
+            0.0f
+        );
+
+    }
+
+    private void Grow() {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+
+        if(collider.tag == "Food") {
+            Grow();
+        } else if (collider.tag == "Obstacle") {
+            ResetState();
+        }     
+
+    }
+
+    private void ResetState() {
+        for(int i = 1; i < _segments.Count; i++) {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        this.transform.position = Vector3.zero;
+
+    }
+
+}
